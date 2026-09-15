34fe6325be5be7ba7b0ed5f5c88a40ea6082d53c
danny_portillo <danny.portillo@cun.edu.co>
2026-09-15 07:34:51 -0500

    Mi primer commit

---
+++ script.js
@@ -0,0 +1,91 @@
+const boton = document.getElementById("calcular");
+
+boton.addEventListener ("click", function() {
+
+    let peso = document.getElementById ("peso") .value;
+    let gravedad = document.getElementById ("planeta") .value;
+
+    let resultado = peso * gravedad;
+
+    document.getElementById ("resultado").textContent =
+        "Tu peso sería " + resultado.toFixed (2) + " kg";
+
+});
+
+const datosPlanetas = {
+
+    Mercurio: {
+        tamano: "4.879 km",
+        composicion: "Roca y metal",
+        lunas: "0",
+        distancia: "57.9 millones km",
+        imagen: "Mercurio.png"
+    },
+
+    Venus: {
+        tamano: "12.104 km",
+        composicion: "Roca y minerales",
+        lunas: "0",
+        distancia: "108.2 millones km"
+    },
+
+    Marte: {
+        tamano: "6.779 km",
+        composicion: "Roca y minerales",
+        lunas: "2",
+        distancia: "227.9 millones km",
+        imagen: "Marte.png"
+    },
+
+    Tierra: {
+        tamano: "12.742 km",
+        composicion: "Roca y minerales",
+        lunas: "1",
+        distancia: "149.6 millones km",
+        imagen: "Tierra.png"
+    },
+
+    Júpiter: {
+        tamano: "139.820 km",
+        composicion: "Gas",
+        lunas: "95",
+        distancia: "778.5 millones km"
+    },
+
+    Saturno: {
+        tamano: "116.460 km",
+        composicion: "Gas",
+        lunas: "146",
+        distancia: "1.434 millones km"
+    },
+
+    Urano: {
+        tamano: "50.724 km",
+        composicion: "Hielo y gas",
+        lunas: "28",
+        distancia: "2.871 millones km"
+    },
+
+    Neptuno: {
+        tamano: "49.244 km",
+        composicion: "Hielo y gas",
+        lunas: "16",
+        distancia: "4.495 millones km"
+    }
+
+};
+
+const selector = document.getElementById("planeta");
+
+selector.addEventListener("change", function() {
+
+    let nombre = selector.options[selector.selectedIndex].text;
+    let datos = datosPlanetas[nombre];
+
+    document.getElementById("nombre-planeta").textContent = nombre;
+    document.getElementById("tamano").textContent = datos.tamano;
+    document.getElementById("composicion").textContent = datos.composicion;
+    document.getElementById("lunas").textContent = datos.lunas;
+    document.getElementById("distancia").textContent = datos.distancia;
+    document.getElementById("imagen-planeta").src = datos.imagen;
+});
